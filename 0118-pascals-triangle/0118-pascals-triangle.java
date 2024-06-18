00c4ccec09e00a5a28892fd098a8efd5cf574a55
Vaibhav Kumar Choudhary <101285210+vkc4@users.noreply.github.com>
2024-06-18 17:39:10 +0530

    Time: 0 ms (100%), Space: 42.1 MB (11.05%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    public static List<Integer> generateRow(int row) {
+    long ans = 1;
+    List<Integer> ansRow = new ArrayList<>();
+    ansRow.add(1); // inserting the 1st element
+
+    // calculate the rest of the elements:
+    for (int col = 1; col < row; col++) {
+        ans = ans * (row - col);
+        ans = ans / col;
+        ansRow.add((int) ans);
+    }
+    return ansRow;
+}
+
+    public List<List<Integer>> generate(int n) {
+         List<List<Integer>> ans = new ArrayList<>();
+
+    // store the entire Pascal's triangle:
+    for (int row = 1; row <= n; row++) {
+        ans.add(generateRow(row));
+    }
+    return ans;
+    }
+}
